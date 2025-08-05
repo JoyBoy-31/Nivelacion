dae33cddec484009c8e763deda4abebe231614a0
rodrigo <wechegaray2024@incos-santacruz.edu.bo>
2025-08-05 19:41:13 -0400

    mi tercer comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'acreens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nivelacion',
+      theme: ThemeData(
+        primaryColor:  Colors.red,
+      ),
+      home: SplashScreen(),
+    );
   }
 }

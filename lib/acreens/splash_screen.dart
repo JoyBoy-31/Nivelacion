c1fc9582118d8d5e893d7d946d5c2f1ee15ea8da
rodrigo <wechegaray2024@incos-santacruz.edu.bo>
2025-08-05 20:35:38 -0400

    Prueba de cocacola

--- lib/acreens/splash_screen.dart
+++ lib/acreens/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'Onboarding_Screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +14,49 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            image: AssetImage("images/pizaa.jpg"),
+            fit: BoxFit.cover,
+            opacity: 0.5,
+          )
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.shopping_cart,
+            size: 250,
+            color: Colors.white),
+            Text("Cocacola",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic
+            ),),
+          ],
+        ),
+      ),
+    );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 5),
+        () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => OnboardingScreen()),
+        ),
+    );
+
+  }
+
 }
